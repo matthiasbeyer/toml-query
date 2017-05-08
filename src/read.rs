274fc6f91eb8a4ee4d0e11e65ef0fd14ee275843
Matthias Beyer <mail@beyermatthias.de>
2017-05-08 13:40:15 +0200

    Add tests for Value::read() interface(s)

--- src/read.rs
+++ src/read.rs
@@ -43,3 +43,153 @@ impl<'doc> TomlValueReadExt<'doc> for Value {
 
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+    use error::*;
+    use toml::from_str as toml_from_str;
+
+    #[test]
+    fn test_read_empty() {
+        let toml : Value = toml_from_str("").unwrap();
+
+        let val  = toml.read_with_seperator(&String::from("a"), '.');
+        assert!(val.is_err());
+        let err = val.unwrap_err();
+
+        assert!(is_match!(err.kind(), &ErrorKind::IdentifierNotFoundInDocument(_)));
+    }
+
+    #[test]
+    fn test_read_table() {
+        let toml : Value = toml_from_str(r#"
+        [table]
+        "#).unwrap();
+
+        let val  = toml.read_with_seperator(&String::from("table"), '.');
+        assert!(val.is_ok());
+        let val = val.unwrap();
+
+        assert!(is_match!(val, &Value::Table(_)));
+        match val {
+            &Value::Table(ref t) => assert!(t.is_empty()),
+            _ => panic!("What just happened?"),
+        }
+    }
+
+    #[test]
+    fn test_read_table_value() {
+        let toml : Value = toml_from_str(r#"
+        [table]
+        a = 1
+        "#).unwrap();
+
+        let val  = toml.read_with_seperator(&String::from("table.a"), '.');
+        assert!(val.is_ok());
+        let val = val.unwrap();
+
+        assert!(is_match!(val, &Value::Integer(1)));
+    }
+
+    #[test]
+    fn test_read_empty_table_value() {
+        let toml : Value = toml_from_str(r#"
+        [table]
+        "#).unwrap();
+
+        let val  = toml.read_with_seperator(&String::from("table.a"), '.');
+        assert!(val.is_err());
+        let err = val.unwrap_err();
+
+        assert!(is_match!(err.kind(), &ErrorKind::IdentifierNotFoundInDocument(_)));
+    }
+
+    #[test]
+    fn test_read_table_index() {
+        let toml : Value = toml_from_str(r#"
+        [table]
+        "#).unwrap();
+
+        let val  = toml.read_with_seperator(&String::from("table.[0]"), '.');
+        assert!(val.is_err());
+        let err = val.unwrap_err();
+
+        assert!(is_match!(err.kind(), &ErrorKind::NoIndexInTable(_)));
+    }
+
+    ///
+    ///
+    /// Querying without specifying the seperator
+    ///
+    ///
+
+    #[test]
+    fn test_read_empty_without_seperator() {
+        let toml : Value = toml_from_str("").unwrap();
+
+        let val  = toml.read(&String::from("a"));
+        assert!(val.is_err());
+        let err = val.unwrap_err();
+
+        assert!(is_match!(err.kind(), &ErrorKind::IdentifierNotFoundInDocument(_)));
+    }
+
+    #[test]
+    fn test_read_table_without_seperator() {
+        let toml : Value = toml_from_str(r#"
+        [table]
+        "#).unwrap();
+
+        let val  = toml.read(&String::from("table"));
+        assert!(val.is_ok());
+        let val = val.unwrap();
+
+        assert!(is_match!(val, &Value::Table(_)));
+        match val {
+            &Value::Table(ref t) => assert!(t.is_empty()),
+            _ => panic!("What just happened?"),
+        }
+    }
+
+    #[test]
+    fn test_read_table_value_without_seperator() {
+        let toml : Value = toml_from_str(r#"
+        [table]
+        a = 1
+        "#).unwrap();
+
+        let val  = toml.read(&String::from("table.a"));
+        assert!(val.is_ok());
+        let val = val.unwrap();
+
+        assert!(is_match!(val, &Value::Integer(1)));
+    }
+
+    #[test]
+    fn test_read_empty_table_value_without_seperator() {
+        let toml : Value = toml_from_str(r#"
+        [table]
+        "#).unwrap();
+
+        let val  = toml.read(&String::from("table.a"));
+        assert!(val.is_err());
+        let err = val.unwrap_err();
+
+        assert!(is_match!(err.kind(), &ErrorKind::IdentifierNotFoundInDocument(_)));
+    }
+
+    #[test]
+    fn test_read_table_index_without_seperator() {
+        let toml : Value = toml_from_str(r#"
+        [table]
+        "#).unwrap();
+
+        let val  = toml.read(&String::from("table.[0]"));
+        assert!(val.is_err());
+        let err = val.unwrap_err();
+
+        assert!(is_match!(err.kind(), &ErrorKind::NoIndexInTable(_)));
+    }
+
+}
+
