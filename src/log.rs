d7dba2178188b60e6e345170a9e8b27a208aee72
Matthias Beyer <mail@beyermatthias.de>
2022-08-09 15:42:59 +0200

    Fix: Add license header
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- src/log.rs
+++ src/log.rs
@@ -1,3 +1,9 @@
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+
 //! If logging is not compiled into the library, this module defines the logging macros to result
 //! in nothing.
 
